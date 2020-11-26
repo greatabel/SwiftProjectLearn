5c8c609ce8ab73c995ea6f58b7b03154f41d9fb6
greatabel <myreceiver2for2github@gmail.com>
2020-11-26 17:48:02 +0800

    change to use storyboard

---
+++ ScreenEvaluator/ScreenEvaluator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenEvaluator
+//
+//  Created by abel on 2020/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
