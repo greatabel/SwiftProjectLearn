c4032ddd3a6bb74692ecabbb8bb8534bb1bc1ccd
greatabel <myreceiver2for2github@gmail.com>
2021-01-04 01:29:21 +0800

    start func

--- b8833running/RunningApp/RunningApp/ViewController.swift
+++ b8833running/RunningApp/RunningApp/ViewController.swift
@@ -1,16 +1,12 @@
-//
-//  ViewController.swift
-//  RunningApp
-//
-//  Created by estedu6 theodore on 29/09/2020.
-//  Copyright © 2020 estedu6 theodore. All rights reserved.
-//
-
 import UIKit
 
 
 class ViewController: UIViewController {
  
+    private var seconds = 0
+    private var timer: Timer?
+    private var distance = Measurement(value: 0, unit: UnitLength.meters)
+    
     @IBOutlet weak var startBtn: UIButton!
     
     @IBOutlet weak var saveBtn: UIButton!
@@ -21,11 +17,50 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var lblDistance: UILabel!
     
+    @IBAction func start_click(_ sender: Any) {
+        startBtn.isHidden = true
+        saveBtn.isHidden = false
+        resumeBtn.isHidden = false
+        
+        seconds = 0
+        distance = Measurement(value: 0, unit: UnitLength.meters)
+//        locationList.removeAll()
+        updateDisplay()
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+          self.eachSecond()
+        }
+    }
+    
+    @IBAction func save_click(_ sender: Any) {
+        
+    }
+    
+    @IBAction func resume_click(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         saveBtn.isHidden = true
         resumeBtn.isHidden = true
+    }
+    
+    func eachSecond() {
+      seconds += 1
+      updateDisplay()
+    }
+    
+    private func updateDisplay() {
+      let formattedDistance = FormatDisplay.distance(distance)
+      let formattedTime = FormatDisplay.time(seconds)
+//      let formattedPace = FormatDisplay.pace(distance: distance,
+//                                             seconds: seconds,
+//                                             outputUnit: UnitSpeed.minutesPerMile)
+      
+        lblDistance.text = "Distance:  \(formattedDistance)"
+        lblTime.text = "Time:  \(formattedTime)"
+//      paceLabel.text = "Pace:  \(formattedPace)"
     }
 
 
