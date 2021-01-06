1787feea51ba837378f1bd72910d45a290ebac21
greatabel <myreceiver2for2github@gmail.com>
2021-01-06 12:08:42 +0800

    a8899 campus_museum

---
+++ a8899_campus_museum/campus_museum/campus_museum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  campus_museum
+//
+//  Created by abel on 2021/1/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
